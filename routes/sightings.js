9486a9fff63a52e81ee07e0a8bf11bbf54aa86fe
nicholasL <nicklohmann@msn.com>
2023-05-12 17:23:06 -0500

    add create sighting functionality

---
+++ routes/sightings.js
@@ -0,0 +1,15 @@
+import { Router } from 'express'
+import { decodeUserFromToken, checkAuth } from '../middleware/auth.js'
+import * as profilesCtrl from '../controllers/profiles.js'
+
+const router = Router()
+
+/*---------- Public Routes ----------*/
+
+
+/*---------- Protected Routes ----------*/
+router.use(decodeUserFromToken)
+router.post('/', checkAuth, sightingsCtrl.create)
+
+
+export { router }
