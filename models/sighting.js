4ef19a5135987e380e97e3670887e6e91233becf
nicholasL <nicklohmann@msn.com>
2023-05-12 16:24:03 -0500

    add Sighting model

---
+++ models/sighting.js
@@ -0,0 +1,33 @@
+import mongoose from 'mongoose'
+
+const Schema = mongoose.Schema
+
+const commentSchema = new Schema(
+  {
+    text: {
+      type: String,
+      required: true
+    },
+    author: { type: Schema.Types.ObjectId, ref: 'Profile' }
+  },
+  { timestamps: true }
+)
+
+const sightingSchema = new Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+    },
+    details: {
+      type: String,
+      required: true,
+    },
+    comments: [commentSchema],
+    author: { type: Schema.Types.ObjectId, ref: 'Profile' }
+  },
+  { timestamps: true }
+)
+const Sighting = mongoose.model('Sighting', sightingSchema)
+
+export { Sighting }
