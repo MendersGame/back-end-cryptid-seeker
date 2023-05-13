c62dac5069ca51a6c201abc1e8ecb41031dc1ba1
MikeE <mike.d.enders@gmail.com>
2023-05-13 10:37:27 -0400

    stub up cryptids routes

---
+++ routes/cryptids.js
@@ -0,0 +1,18 @@
+import { Router } from 'express'
+import * as cryptidsCtrl from '../controllers/cryptids.js'
+import { decodeUserFromToken, checkAuth } from '../middleware/auth.js'
+
+const router = Router()
+
+/*---------- Public Routes ----------*/
+
+
+/*---------- Protected Routes ----------*/
+router.use(decodeUserFromToken)
+router.post('/', checkAuth, cryptidsCtrl.create)
+router.get('/', checkAuth, cryptidsCtrl.index)
+router.get('/:cryptidId', checkAuth, cryptidsCtrl.show)
+router.put('/:cryptidId', checkAuth, cryptidsCtrl.update)
+router.delete('/:cryptidId', checkAuth, cryptidsCtrl.delete)
+
+export { router }
