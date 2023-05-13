6fe4b063cec179de5a8d211f699d073d9d7657dd
MikeE <mike.d.enders@gmail.com>
2023-05-13 14:55:05 -0400

    remove profile fields from cryptid model

--- controllers/cryptids.js
+++ controllers/cryptids.js
@@ -5,12 +5,12 @@ async function create(req, res) {
   try {
     req.body.author = req.user.profile;
     const cryptid = await Cryptid.create(req.body);
-    const profile = await Profile.findByIdAndUpdate(
-      req.user.profile,
-      { $push: { cryptids: cryptid } },
-      { new: true }
-    )
-    cryptid.author = profile
+    // const profile = await Profile.findByIdAndUpdate(
+    //   req.user.profile,
+    //   { $push: { cryptids: cryptid } },
+    //   { new: true }
+    // )
+    // cryptid.author = profile
     res.status(201).json(cryptid)
   } catch (error) {
     console.log(error)
@@ -21,7 +21,7 @@ async function create(req, res) {
 async function index(req, res) {
   try {
     const cryptid = await Cryptid.find({})
-    .populate('author')
+    // .populate('author')
     .sort({createdAt: 'desc'})
     res.status(200).json(cryptid)
   } catch (error) {
@@ -33,7 +33,7 @@ async function index(req, res) {
 async function show(req, res) {
   try {
     const cryptid = await Cryptid.findById(req.params.cryptidId)
-      .populate(['author'])
+      // .populate(['author'])
     res.status(200).json(cryptid)
   } catch (error) {
     console.log(error);
@@ -47,7 +47,8 @@ async function update(req, res) {
       req.params.cryptidId, 
       req.body, 
       { new: true }
-    ).populate('author')
+    )
+    // .populate('author')
     res.status(200).json(cryptid)
   } catch (error) {
     console.log(error);
@@ -56,12 +57,22 @@ async function update(req, res) {
 }
 
 //todo come back later to impelement delete functionality
+// async function deleteCryptid(req, res) {
+//   try {
+//     const cryptid = await Cryptid.findByIdAndDelete(req.params.cryptidId)
+//     cryptid.remove({_id: req.params.cryptidId })
+//     await cryptid.save()
+//     res.status(200).json(cryptid)
+//   } catch (error) {
+//     console.log(error);
+//     res.status(500).json(error)
+//   }
+// }
+
 async function deleteCryptid(req, res) {
   try {
     const cryptid = await Cryptid.findByIdAndDelete(req.params.cryptidId)
-    const profile = await Profile.findById(req.user.profile)
-    profile.cryptids.remove({_id: req.params.cryptidId })
-    await profile.save()
+    await cryptid.save()
     res.status(200).json(cryptid)
   } catch (error) {
     console.log(error);
