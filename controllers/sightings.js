63ff21ec23dc5c0fa2158a1ab123526c5165493a
nicholasL <nicklohmann@msn.com>
2023-05-13 23:27:18 -0500

    update sightings create controller

--- controllers/sightings.js
+++ controllers/sightings.js
@@ -2,30 +2,39 @@ import { Profile } from "../models/profile.js";
 import { Sighting } from "../models/sighting.js";
 import { Cryptid } from "../models/cryptid.js";
 
+
 async function create(req, res) {
   try {
+    const cryptid = await Cryptid.findOne({ name: req.body.cryptid });
+
+    if (!cryptid) {
+      return res.status(404).json({ message: 'Cryptid not found' });
+    }
+
+    req.body.cryptid = cryptid._id
     req.body.author = req.user.profile;
-    req.body.cryptid = req.Cryptid._id
+
     const sighting = await Sighting.create(req.body);
+
     const profile = await Profile.findByIdAndUpdate(
       req.user.profile,
       { $push: { sightings: sighting } },
       { new: true }
-    )
-    const cryptid = await Cryptid.findByIdAndUpdate(
-      req.Cryptid._id,
-      { $push: { sightings: sighting} },
-      { new: true }
-    )
+    );
 
-    sighting.author = profile
-    sighting.cryptid = cryptid
-    res.status(201).json(sighting)
+    await Cryptid.findOneAndUpdate(
+      {name: cryptid.name},
+      { $push: { sightings: sighting } },
+      { new: true }
+    );
+    console.log(cryptid.name);
+    res.status(201).json(sighting);
   } catch (error) {
-    console.log(error)
-    res.status(500).json(error)
+    console.log(error);
+    res.status(500).json(error);
   }
 }
+
 
 async function index(req, res) {
   try {
