d21dfdc2c820f0811b9763deea628c3a0d084fc7
nicholasL <nicklohmann@msn.com>
2023-05-13 23:32:44 -0500

    removed semicolons for consistent code

--- controllers/sightings.js
+++ controllers/sightings.js
@@ -1,41 +1,41 @@
-import { Profile } from "../models/profile.js";
-import { Sighting } from "../models/sighting.js";
-import { Cryptid } from "../models/cryptid.js";
+import { Profile } from "../models/profile.js"
+import { Sighting } from "../models/sighting.js"
+import { Cryptid } from "../models/cryptid.js"
 
 
 async function create(req, res) {
   try {
     // Find the Cryptid that matches the name passed in the request body
-    const cryptid = await Cryptid.findOne({ name: req.body.cryptid });
+    const cryptid = await Cryptid.findOne({ name: req.body.cryptid })
 
     // If no matching Cryptid is found, return an error response
     if (!cryptid) {
-      return res.status(404).json({ message: 'Cryptid not found' });
+      return res.status(404).json({ message: 'Cryptid not found' })
     }
     // Update the request body with the ID of the Cryptid that was found and the ID of the user making the request
     req.body.cryptid = cryptid._id
-    req.body.author = req.user.profile;
+    req.body.author = req.user.profile
 
      // Create a new Sighting using the updated request body
-    const sighting = await Sighting.create(req.body);
+    const sighting = await Sighting.create(req.body)
 
     // Update the user's Profile by adding the newly created Sighting to the "sightings" array
     const profile = await Profile.findByIdAndUpdate(
       req.user.profile,
       { $push: { sightings: sighting } },
       { new: true }
-    );
+    )
     // Update the Cryptid that was found by adding the newly created Sighting to the "sightings" array
     await Cryptid.findOneAndUpdate(
       {name: cryptid.name},
       { $push: { sightings: sighting } },
       { new: true }
-    );
-    console.log(cryptid.name);
-    res.status(201).json(sighting);
+    )
+    console.log(cryptid.name)
+    res.status(201).json(sighting)
   } catch (error) {
-    console.log(error);
-    res.status(500).json(error);
+    console.log(error)
+    res.status(500).json(error)
   }
 }
 
@@ -58,7 +58,7 @@ async function show(req, res) {
       .populate(['author', 'comments.author'])
     res.status(200).json(sighting)
   } catch (error) {
-    console.log(error);
+    console.log(error)
     res.status(500).json(error)
   }
 }
@@ -72,7 +72,7 @@ async function update(req, res) {
     ).populate('author')
     res.status(200).json(sighting)
   } catch (error) {
-    console.log(error);
+    console.log(error)
     res.status(500).json(error)
   }
 }
@@ -85,7 +85,7 @@ async function deleteSighting(req, res) {
     await profile.save()
     res.status(200).json(sighting)
   } catch (error) {
-    console.log(error);
+    console.log(error)
     res.status(500).json(error)
   }
 }
@@ -104,7 +104,7 @@ async function createComment(req, res) {
     
     res.status(201).json(newComment)
   } catch (error) {
-    console.log(error);
+    console.log(error)
     res.status(500).json(error)
   }
 }
