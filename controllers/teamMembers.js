8c3f20bd0eb0bb74fa4b642e2464271625262518
MikeE <mike.d.enders@gmail.com>
2023-05-15 16:07:35 -0400

    create team member controller

---
+++ controllers/teamMembers.js
@@ -0,0 +1,16 @@
+import { TeamMember } from "../models/teamMember"
+
+async function create(req, res) {
+  try {
+    req.body.author = req.user.profile;
+    const teamMember = await TeamMember.create(req.body);
+    res.status(201).json(teamMember)
+  } catch (error) {
+    console.log(error)
+    res.status(500).json(error)
+  }
+}
+
+export {
+  create
+}
